0efd1a844d8dce07b4a15efcb3e8c51f0cbbe370
lazarus0320 <astar5327z@gmail.com>
2024-09-01 22:30:14 +0900

    Fix: 누락된 기술 스택 뱃지 복구

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -1,9 +1,52 @@
-import { writeFileSync } from 'node:fs';
-import Parser from 'rss-parser';
+const fs = require('fs');
+const dayjs = require('dayjs');
+const Parser = require('rss-parser');
+const timezone = require('dayjs/plugin/timezone');
+const utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Seoul');
 
-let text = `##최근 게시글
+let text = `
+### Hi there 👋
+
+[![lazarus0320's github stats](https://github-readme-stats.vercel.app/api?username=lazarus0320&theme=tokyonight&show_icons=true)](https://github.com/anuraghazra/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=lazarus0320&layout=compact&theme=tokyonight)](https://github.com/anuraghazra/github-readme-stats)
+
+[![solved.ac tier](http://mazassumnida.wtf/api/generate_badge?boj=lazarus0320)](https://solved.ac/lazarus0320)
+
+---
+
+### 🛠️ Tech Stack
+
+#### Backend
+![Java](https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white)
+![SpringBoot](https://img.shields.io/badge/SpringBoot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white)
+![MySQL](https://img.shields.io/badge/MySQL-4479A1?style=for-the-badge&logo=mysql&logoColor=white)
+![Postgres](https://img.shields.io/badge/PostgreSQL-336791?style=for-the-badge&logo=postgresql&logoColor=white)
+![Redis](https://img.shields.io/badge/Redis-DC382D?style=for-the-badge&logo=redis&logoColor=white)
+
+#### DevOps & Tools
+![Docker](https://img.shields.io/badge/Docker-2496ED?style=for-the-badge&logo=docker&logoColor=white)
+![GitHub Actions](https://img.shields.io/badge/GitHub_Actions-2088FF?style=for-the-badge&logo=github-actions&logoColor=white)
+![AWS EC2](https://img.shields.io/badge/AWS_EC2-FF9900?style=for-the-badge&logo=amazon-aws&logoColor=white)
+![AWS RDS](https://img.shields.io/badge/AWS_RDS-527FFF?style=for-the-badge&logo=amazon-rds&logoColor=white)
+![Ubuntu](https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white)
+
+#### Frontend & UI/UX
+![React](https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black)
+![Flutter](https://img.shields.io/badge/Flutter-02569B?style=for-the-badge&logo=flutter&logoColor=white)
+![Figma](https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white)
+
+#### Collaboration & Documentation
+![Notion](https://img.shields.io/badge/Notion-000000?style=for-the-badge&logo=notion&logoColor=white)
+![Jira](https://img.shields.io/badge/Jira-0052CC?style=for-the-badge&logo=jira&logoColor=white)
+
+---
+
+### 📖 Latest Blog Posts
 `;
-// rss-parser 생성
+
 const parser = new Parser({
   headers: {
     Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
@@ -12,21 +55,22 @@ const parser = new Parser({
 
 (async () => {
   // 피드 목록
-  const feed = await parser.parseURL(
-    'https://afterdawncoding.tistory.com//rss'
-  );
+  const feed = await parser.parseURL('https://afterdawncoding.tistory.com/rss');
 
   // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
   for (let i = 0; i < 5; i++) {
-    const { title, link } = feed.items[i];
+    const { title, link, pubDate } = feed.items[i];
     console.log(`${i + 1}번째 게시물`);
     console.log(`추가될 제목: ${title}`);
     console.log(`추가될 링크: ${link}`);
-    text += `<a href=${link}>${title}</a></br>`;
+
+    const date = dayjs(pubDate).add(9, 'hours').format('YYYY.MM.DD HH:mm:ss');
+    text += `<a href="${link}">${title}</a></br>`;
+    text += `게시일자 : ${date}</br></br>`;
   }
 
   // README.md 파일 작성
-  writeFileSync('README.md', text, 'utf8', (e) => {
+  fs.writeFileSync('README.md', text, 'utf8', (e) => {
     console.log(e);
   });
 
